0b0bcc1c16a28d63965ad82070b188d5c946ebbe
Erle Carrara <carrara.erle@gmail.com>
2018-10-29 14:39:16 -0300

    Permite especificar fator de escalonamento

--- celeryasg/cli.py
+++ celeryasg/cli.py
@@ -3,27 +3,35 @@
 Celery ASG
 
 Usage:
-  celery-asg --asg-name=<auto-scaling-group-name> --broker=<celery-broker-url> [--queue=<queue-name>] [--dry-run]
+  celery-asg --asg-name=<auto-scaling-group-name> --broker=<celery-broker-url> [--queue=<queue-name>] [--factor=<n>] [--dry-run]
 
 Options:
   -h --help      Show this screen.
   --version      Show version.
 """
 
+import sys
 from docopt import docopt
 from celeryasg import __version__
 from celeryasg.core import CeleryASG
 
 
-def run(asg_name, broker, queue_name, dryrun=False):
+def run(asg_name, broker, queue_name, factor=0.5, dryrun=False):
     celery = CeleryASG(asg_name=asg_name, broker=broker)
     inactive_instances = celery.find_inactive_instances()
     for instance in inactive_instances:
         print('Shuting down: {}'.format(instance['InstanceId']))
         celery.shutdown_instance(instance, dryrun=dryrun)
 
+    try:
+        factor = float(factor)
+    except ValueError:
+        print('Invalid factor!')
+        sys.exit(1)
+        return
+
     print('Auto balancing ASG...')
-    n = celery.auto_balance(dryrun=dryrun)
+    n = celery.auto_balance(factor=factor, dryrun=dryrun)
     if n:
         print('Desired changed to {}'.format(n))
 
@@ -34,6 +42,7 @@ def entrypoint():
     run(args['--asg-name'],
         args['--broker'],
         args['--queue'],
+        args['--factor'],
         args['--dry-run'])
 
 
